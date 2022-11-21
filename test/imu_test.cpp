700d7595c7debfc942ed7b5a1c83cc446c5d4ff6
Lewis Kaye <lewiskaye@hotmail.co.uk>
2022-11-21 17:13:06 +0000

    Adding testing support with Google GTest

---
+++ test/imu_test.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+// SanityCheck - should always return true
+TEST(IMUTestSuite, SanityCheck) {
+    EXPECT_TRUE(true);
+}
---
+++ test/lidar_converter_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+// SanityCheck - should always return true
+TEST(LidarConverterTestSuite, SanityCheck) {
+    EXPECT_TRUE(true);
+}
+
+//
