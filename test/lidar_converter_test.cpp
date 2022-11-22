e53b21d67ce70cf1471f8a6a99bdc297cdec7dd8
Lewis Kaye <lewiskaye@hotmail.co.uk>
2022-11-22 03:16:20 +0000

    2nd Point Cloud Test

--- test/lidar_converter_test.cpp
+++ test/lidar_converter_test.cpp
@@ -7,13 +7,6 @@ TEST(LidarConverterTestSuite, SanityCheck) {
 }
 
 
-TEST(LidarConverterTestSuite, Test1) {
-    //PointCloudPublisher* pc_publisher = new PointCloudPublisher();
-    //pc_publisher.publish_pointcloud();
-    
-    EXPECT_TRUE(true);
-}
-
 sensor_msgs::msg::LaserScan generate_random_laserscan() {
     sensor_msgs::msg::LaserScan scan;
     unsigned int num_readings = 4;
@@ -50,24 +43,21 @@ sensor_msgs::msg::LaserScan generate_test_laserscan(double ranges[], unsigned in
     //unsigned int num_readings = 4;
     double laser_frequency = 50;
     //double ranges[] = {1,2,3,4};
-    double intensities[] = {100,100,100,100};
-    int count = 0;
+    //double intensities[] = {100,100,100,100};
 
     scan.header.frame_id = "laser_frame";
-    scan.angle_min = -180 * (3.14/180);
-    scan.angle_max = 180 * (3.14/180);
-    scan.angle_increment = 360/num_readings * (3.14/180);
+    scan.angle_min = -180 * (3.14159/180);
+    scan.angle_max = 180 * (3.14159/180);
+    scan.angle_increment = 360/num_readings * (3.14159/180);
     scan.time_increment = (1 / laser_frequency) / (num_readings);
     scan.range_min = 0.02;
     scan.range_max = 12.0;
     scan.ranges.resize(num_readings);
     scan.intensities.resize(num_readings);
-
     for(unsigned int i = 0; i < num_readings; ++i)
     {
       scan.ranges[i] = ranges[i];
-      scan.intensities[i] = intensities[i];
-      //std::cout << std::to_string(scan.ranges[i]) << ", "; //DEBUG
+      scan.intensities[i] = 100;
     }
 
     return scan;
@@ -87,31 +77,36 @@ pcl::PointCloud<pcl::PointXYZI> laserscan_to_pcl(sensor_msgs::msg::LaserScan sca
     return pcl;
 }
 
+void PrintPointCloud(pcl::PointCloud<pcl::PointXYZI> cloud){
+    for(auto &pt : cloud.points) {
+        printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+    }
+}
+
 TEST(LidarConverterTestSuite, TestLaserScanToPointCloud2) {
-    // Generate fake scan message with fake point
+    // Generate fake scan message with fake points
     double test_ranges[] = {1,2,3,4};
+    // PASS, PASS, PASS, PASS
     sensor_msgs::msg::LaserScan scan = generate_test_laserscan(test_ranges, 4);
     pcl::PointCloud<pcl::PointXYZI> pcl = laserscan_to_pcl(scan);
-
-    /*printf ("Cloud: width = %d, height = %d\n", cloud.width, cloud.height);
-    for(auto &pt : pcl.points) {
-        printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
-    }*///DEBUG
 
     EXPECT_NEAR(pcl.points[0].x, -test_ranges[0], 0.1);
     EXPECT_NEAR(pcl.points[1].y, -test_ranges[1], 0.1);
     EXPECT_NEAR(pcl.points[2].x, test_ranges[2], 0.1);
     EXPECT_NEAR(pcl.points[3].y, test_ranges[3], 0.1);
-
-    for(auto &pt : pcl.points) {
-        //printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
-        //EXPECT_NEAR(pt.x, -test_ranges[0], 0.1);
-    }
 }
 
-// Tests LaserScan to PointCloud2 by checking a point on them?
+TEST(LidarConverterTestSuite, TestAbnormalValues) {
+    // Generate fake scan message with fake points
+    double test_ranges[] = {4.1321564,-4,13,99999};
+    // PASS, FAIL, FAIL, FAIL
+    sensor_msgs::msg::LaserScan scan = generate_test_laserscan(test_ranges, 4);
+    pcl::PointCloud<pcl::PointXYZI> pcl = laserscan_to_pcl(scan);
 
+    PrintPointCloud(pcl);
+    EXPECT_NEAR(pcl.points[0].x, -test_ranges[0], 0.1);
+    EXPECT_NE(pcl.points[1].y, -test_ranges[1]);
+    EXPECT_NE(pcl.points[2].x, test_ranges[2]);
+    EXPECT_NE(pcl.points[3].y, test_ranges[3]);
+}
 
-// Scan callback empty?
-
-// Check project laser returns something?
